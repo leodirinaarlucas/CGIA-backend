c8f1611aa5ff1e8d7fa8eddbeb3b1b04303b986c
Artur Carneiro <csf.artur@gmail.com>
2019-10-06 15:27:53 -0300

    Modified database config for Heroku integration.

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -18,8 +18,15 @@ public func configure(
 
     // Configure the rest of your application here
     
-    let postgreSQLConfig = PostgreSQLDatabaseConfig(hostname: "127.0.0.1", port: 5432, username: "carneiro", database: "CGIAdb", password: nil)
-    services.register(postgreSQLConfig)
+    if let url = Environment.get("DATABASE_URL") {
+        if let postgreSQLConfig = PostgreSQLDatabaseConfig(url: url) {
+            services.register(postgreSQLConfig)
+        }
+    } else {
+        let postgreSQLConfig = PostgreSQLDatabaseConfig(hostname: "127.0.0.1", port: 5432, username: "carneiro", database: "CGIAdb", password: nil)
+        services.register(postgreSQLConfig)
+    }
+    
     
     var migrations = MigrationConfig()
     migrations.add(model: Student.self, database: .psql)
