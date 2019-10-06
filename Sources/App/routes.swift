ab47471184f7de732b6362c67ec0e7c959eb1bb3
Artur Carneiro <csf.artur@gmail.com>
2019-10-06 13:38:23 -0300

    Added POST and GET routes to Students, Instructor and Course.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,12 @@ public func routes(_ router: Router) throws {
     let studentController = StudentController()
     router.get("students", use: studentController.get)
     router.post("students","add", use: studentController.post)
+    
+    let instructorController = InstructorController()
+    router.get("instructor", use: instructorController.get)
+    router.post("instructor", "add", use: instructorController.post)
+    
+    let courseController = CourseController()
+    router.get("course", use: courseController.get)
+    router.post("course", "add", use: courseController.post)
 }
